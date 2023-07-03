0835d9c230c5e9632bdaaa3cb88ce3910ea32ee2
Josepablo C <josepablo134@gmail.com>
2023-07-03 16:31:11 -0600

    feat(main_h0): Add counter to core 0

--- project/Harts/src/Hart0.c
+++ project/Harts/src/Hart0.c
@@ -1,19 +1,23 @@
 #include "Harts.h"
 
+#define MAX_COUNTER_VAL 0xFFFF
+
 int main_h0(void){
-    int data;
+    unsigned int counter;
     printf("Core 0: Hello world\n");
 
     // Enable Hardware Thread 1
         clint_ipi_send( 1u );
         atomic_set(&g_wake_up[1u], 1);
 
-    /* Clear stdin buffer before scanf */
-    sys_stdin_flush();
-
-    scanf("%d", &data);
-    printf("\nData is %d\n", data);
-    while(1);
+    while(1){
+        counter++;
+        if( counter == MAX_COUNTER_VAL ){
+            counter = 0;
+        }
+        printf("Core 0: Counter %u\n",counter);
+        sleep(1);
+    }
 
     return 0;
 }
